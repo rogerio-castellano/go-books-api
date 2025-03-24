dc8e15ae2f3743015027c8e709afab463b2c6c85
Rogerio Castellano <rc_publico-github@yahoo.com.br>
2025-03-24 12:34:53 +0100

    Change BookList to view books from a dummy array of books

--- frontend/src/Components/BooksList.tsx
+++ frontend/src/Components/BooksList.tsx
@@ -1,4 +1,13 @@
+import Book from "../Book.model";
+
 export const BooksList = () => {
+  const Books: Book[] = [
+    { id: 1, title: "The Go Programming Language", author: "Alan Donovan", pages: 400 },
+    { id: 2, title: "Clean Code", author: "Robert C. Martin", pages: 464 },
+    { id: 3, title: "You Don't Know JS", author: "Kyle Simpson", pages: 278 },
+    { id: 4, title: "Design Patterns", author: "Erich Gamma", pages: 395 },
+  ];
+
   return (
     <section id="book-list">
       <h2>Book List</h2>
@@ -13,16 +22,18 @@ export const BooksList = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Book Title</td>
-            <td>Book Author</td>
-            <td>350</td>
-            <td>
-              <button>Edit</button>
-              <button>Delete</button>
-            </td>
-          </tr>
+          {Books.map((book) => (
+            <tr key={book.id}>
+              <td>{book.id}</td>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.pages}</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </section>
