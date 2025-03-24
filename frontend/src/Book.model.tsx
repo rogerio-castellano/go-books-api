c1e7304c407acb27a1b50777a0a4bca86514c167
Rogerio Castellano <rc_publico-github@yahoo.com.br>
2025-03-24 12:26:52 +0100

    Add Book interface

---
+++ frontend/src/Book.model.tsx
@@ -0,0 +1,6 @@
+export default interface Book {
+  id: number; // Unique identifier for the book
+  title: string; // Title of the book
+  author: string; // Author of the book
+  pages: number; // Number of pages in the book
+}
