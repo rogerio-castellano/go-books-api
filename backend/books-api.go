aabd948652cf2590435051e06b684c6372f9ce57
Rogerio Castellano <rc_publico-github@yahoo.com.br>
2025-03-24 13:46:21 +0100

    Change API header to allow requests from the frontend

--- backend/books-api.go
+++ backend/books-api.go
@@ -31,9 +31,9 @@ func main() {
 }
 
 func bookHandler(w http.ResponseWriter, r *http.Request) {
-	// w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
-	// w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-	// w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	switch r.Method {
 	case "GET":
 		handleGetBooks(w, r)
