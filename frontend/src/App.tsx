155b2c46ac7a2cb6c21b9df072a52f847a304291
Rogerio Castellano <rc_publico-github@yahoo.com.br>
2025-03-24 13:30:12 +0100

    Implement Add books feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,28 @@ import "./App.css";
 import Book from "./Book.model";
 import { BooksForm } from "./Components/BooksForm";
 import { BooksList } from "./Components/BooksList";
+import { useState } from "react";
+
+const initialBooks: Book[] = [
+  { id: 1, title: "The Go Programming Language", author: "Alan Donovan", pages: 400 },
+  { id: 2, title: "Clean Code", author: "Robert C. Martin", pages: 464 },
+  { id: 3, title: "You Don't Know JS", author: "Kyle Simpson", pages: 278 },
+  { id: 4, title: "Design Patterns", author: "Erich Gamma", pages: 395 },
+];
 
 function App() {
-  const books: Book[] = [
-    { id: 1, title: "The Go Programming Language", author: "Alan Donovan", pages: 400 },
-    { id: 2, title: "Clean Code", author: "Robert C. Martin", pages: 464 },
-    { id: 3, title: "You Don't Know JS", author: "Kyle Simpson", pages: 278 },
-    { id: 4, title: "Design Patterns", author: "Erich Gamma", pages: 395 },
-  ];
+  const [books, setBooks] = useState<Book[]>(initialBooks);
+
+  function addBook(book: Book): void {
+    books.push(book);
+    setBooks([...books]);
+    console.log(books);
+  }
 
   return (
     <div className="App">
       <main>
-        <BooksForm />
+        <BooksForm onSubmittedBook={(book) => addBook(book)} />
         <BooksList books={books} />
       </main>
     </div>
--- frontend/src/Components/BooksForm.tsx
+++ frontend/src/Components/BooksForm.tsx
@@ -1,16 +1,30 @@
-export const BooksForm = () => {
+import Book from "../Book.model";
+import { useForm } from "react-hook-form";
+
+interface Props {
+  onSubmittedBook: (book: Book) => void;
+}
+
+export const BooksForm = ({ onSubmittedBook }: Props) => {
+  const { register, handleSubmit, reset } = useForm<Book>();
+
   return (
     <section id="book-form">
       <h2>Add a Book</h2>
-      <form id="addBookForm">
+      <form
+        onSubmit={handleSubmit((data) => {
+          onSubmittedBook(data);
+          reset();
+        })}
+      >
         <label htmlFor="title">Title:</label>
-        <input type="text" id="title" name="title" placeholder="Enter book title" required />
+        <input type="text" id="title" placeholder="Enter book title" required {...register("title")} />
 
         <label htmlFor="author">Author:</label>
-        <input type="text" id="author" name="author" placeholder="Enter author's name" required />
+        <input type="text" id="author" placeholder="Enter author's name" required {...register("author")} />
 
         <label htmlFor="pages">Pages:</label>
-        <input type="number" id="pages" name="pages" placeholder="Enter page count" required />
+        <input type="number" id="pages" placeholder="Enter page count" required {...register("pages")} />
 
         <button type="submit">Add Book</button>
       </form>
